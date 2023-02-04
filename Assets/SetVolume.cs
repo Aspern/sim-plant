a78f90b195d20cf15b2767251a11311375c91088
Thomas <6321074+t40mas@users.noreply.github.com>
2023-02-04 21:55:56 +0100

    just comment

--- Assets/SetVolume.cs
+++ Assets/SetVolume.cs
@@ -8,7 +8,8 @@ public class SetVolume : MonoBehaviour
     public AudioMixer mixer;
     public Slider slider;
     void Start() {
-        slider.value = PlayerPrefs.GetFloat("MusicVol", 0.75f);
+        // sets default volume level
+        slider.value = PlayerPrefs.GetFloat("MusicVol", 0.1f);
     }
     public void SetLevel() {
         float sliderValue = slider.value;
