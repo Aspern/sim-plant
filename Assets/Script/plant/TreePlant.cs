ecd0ca94021439f3d5ff4b100f5a59455dc9a769
Adrian Weber <adrian@kuebler-weber.de>
2023-02-04 18:39:57 +0100

    restrict actions dependen on plant status

--- Assets/Script/plant/TreePlant.cs
+++ Assets/Script/plant/TreePlant.cs
@@ -3,8 +3,19 @@
 public class TreePlant :  MonoBehaviour
 {
     public float initialGrowTime = 60f;
+
+    private void Awake()
+    {
+        transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+    }
+
     private void Start()
     {
-        LeanTween.scale(gameObject, new Vector3(1, 1, 1), initialGrowTime);
+        LeanTween.scale(gameObject, new Vector3(1, 1, 1), initialGrowTime).setOnComplete(() =>
+        {
+            gameObject.GetComponentInParent<Tile>().PlantGrown = true;
+        });
     }
+
+ 
 }
