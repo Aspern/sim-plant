65a6d9b93ecfb0ae9bd48f13a2cd8f80ffa9ecb6
Adrian Weber <adrian@kuebler-weber.de>
2023-02-04 14:07:01 +0100

    Add selection logic on tile map

--- Assets/Script/tile/TileMap.cs
+++ Assets/Script/tile/TileMap.cs
@@ -3,37 +3,29 @@ using UnityEngine;
 
 public class TileMap : MonoBehaviour
 {
-    private readonly int directions = 4;
     private Tile[,] _tiles;
+    private Tile _selection;
 
     private void Awake()
     {
         var mapData = GameObject.Find("Map").GetComponent<MapData>();
         _tiles = new Tile [mapData.data.GetLength(0), mapData.data.GetLength(1)];
-        
-        for (var i = 0; i < mapData.data.GetLength(0); i++)
-        {
-            for (var j = 0; j < mapData.data.GetLength(1); j++)
-            {
-                var tileId = mapData.data[i, j] / directions;
-                try
-                {
-                    var tile = new Tile
-                    {
-                        type = TileTypes.findById(tileId)
-                    };
-                    _tiles[i, j] = tile;
-                }
-                catch (Exception e)
-                {
-                    Debug.Log(e);
-                }
-            }
-        }
     }
 
-    public Tile getByPosition(int x, int y)
+    public void AddTile(int x, int y, Tile tile)
     {
-        return _tiles[x, y];
+        _tiles[x, y] = tile;
+    }
+
+    public void SelectTile(Tile tile)
+    {
+        _selection = tile;
+        Debug.Log($"Selected tile  {tile.type.id}, {tile.type.name}");
+    }
+
+    public void UnselectTile()
+    {
+        _selection = null;
+        Debug.Log("Unselected tile");
     }
 }
--- Assets/Script/tile/TileTypes.cs
+++ Assets/Script/tile/TileTypes.cs
@@ -9,6 +9,8 @@ public static class TileTypes
     public static TileType Corner = new(2, "corner");
     public static TileType InnerCorner = new(3, "inner_corner");
     public static TileType Plain = new(4, "plain");
+    public static TileType Rock = new(3, "rock");
+    public static TileType Tree = new(4, "tree");
 
     
     public static TileType findById(int id)
@@ -20,6 +22,8 @@ public static class TileTypes
             2 => Corner,
             3 => InnerCorner,
             4 => Plain,
+            5 => Rock,
+            6 => Tree,
             _ => throw new Exception("Cannot find tile for id " + id)
         };
     }
