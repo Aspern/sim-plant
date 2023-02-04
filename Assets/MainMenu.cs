032eaab94a210d245568f25b4aa5f1392241da58
Thomas <6321074+t40mas@users.noreply.github.com>
2023-02-04 18:40:20 +0100

    color & sound

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer mixer;
     public void PlayGame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
@@ -12,5 +14,10 @@ public class MainMenu : MonoBehaviour
     public void QuitGame(){
         Debug.Log("QUIT!");
         Application.Quit();
+    }
+
+    public void Awake (){
+        Debug.Log("Hallo Welt");
+         mixer.SetFloat("MusicVol", Mathf.Log10(0.5f) * 20);
     }
 }
