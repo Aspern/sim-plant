0fceef4a86d438f3c3d5a8d0c6dd726ade87ae34
Thomas <6321074+t40mas@users.noreply.github.com>
2023-02-04 21:51:53 +0100

    save sound volume

--- Assets/SetVolume.cs
+++ Assets/SetVolume.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-
-public class SetVolume : MonoBehaviour {
+using UnityEngine.UI;
+public class SetVolume : MonoBehaviour
+{
     public AudioMixer mixer;
-    public void SetLevel (float sliderValue) {
+    public Slider slider;
+    void Start() {
+        slider.value = PlayerPrefs.GetFloat("MusicVol", 0.75f);
+    }
+    public void SetLevel() {
+        float sliderValue = slider.value;
         mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MusicVol", sliderValue);
     }
 }
