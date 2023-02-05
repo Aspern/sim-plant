ac212daada01f88c6210cb130d0f32762a4d8814
Adrian Weber <adrian@kuebler-weber.de>
2023-02-05 12:48:33 +0100

    Implement plant counter

---
+++ Assets/Script/hud/PlantCounter.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class PlantCounter  : MonoBehaviour
+{
+    public TextMeshProUGUI counterText;
+
+    public void SetCounter(int plants, int maxPlants)
+    {
+        counterText.SetText($"{plants} / {maxPlants}");
+    }
+}
