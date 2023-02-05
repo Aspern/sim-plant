d64dfcb2da5f477f9b9ac85ad3d154099291661e
Adrian Weber <adrian@kuebler-weber.de>
2023-02-05 14:12:58 +0100

    Optimize Compass

--- Assets/Script/hud/Compass.cs
+++ Assets/Script/hud/Compass.cs
@@ -1,28 +1,24 @@
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Compass : MonoBehaviour {
-
-    private RectTransform _rt;
-    private int _currentDirection;
-
-    public int CurrentDirection {
-        get { return _currentDirection; }
-    }
     
+    private RectTransform _compassNeedle;
+
+    public int CurrentDirection { get; private set; }
+
     private void Awake() {
-        _rt = gameObject.GetComponent<RectTransform>();
+        _compassNeedle = GameObject.Find("CompassNeedle").GetComponent<RectTransform>();
     }
 
-    void Start() {
-        _currentDirection = 0;
-        LeanTween.delayedCall(Random.Range(5f, 10f), callback);
+    private void Start() {
+        CurrentDirection = 0;
+        LeanTween.delayedCall(Random.Range(5f, 10f), Callback);
     }
 
-    void callback()
+    private void Callback()
     {
-        _currentDirection = Random.Range(0, 8);
-        _rt.transform.rotation = Quaternion.Euler(0, 0, 45 * _currentDirection);
-        LeanTween.delayedCall(Random.Range(5f, 10f), callback);
+        CurrentDirection = Random.Range(0, 8);
+        _compassNeedle.transform.rotation = Quaternion.Euler(0, 0, 45 * CurrentDirection);
+        LeanTween.delayedCall(Random.Range(5f, 10f), Callback);
     }
 }
